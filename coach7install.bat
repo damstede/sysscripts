ca64ac9af282951c040acb45e2bedab113f84df5
Freek Bes <freekbladnl@gmail.com>
2022-05-16 15:24:59 +0200

    kleine verbeteringen in user input

--- coach7install.bat
+++ coach7install.bat
@@ -3,10 +3,10 @@
 SET COACHINSTALLER=%cd%\coach7_7.msi
 IF NOT EXIST %COACHINSTALLER% SET /p COACHINSTALLER="Bestand coach7_7.msi niet gevonden. Geef de locatie van het Coach 7 installatieprogramma op: "
 
-ECHO "Vul de licentiegegevens van jouw school of organisatie in."
+ECHO Vul de licentiegegevens van jouw school of organisatie in.
 
-SET /p LICENSENAME="Naam van de licentie: "
+SET /p LICENSENAME="Naam: "
 
-SET /p LICENSECODE="Code van de licentie: "
+SET /p LICENSECODE="Code: "
 
 MSIEXEC /i "%COACHINSTALLER%" LICENSENAME="%LICENSENAME%" LICENSECODE="%LICENSECODE%" LANGUAGECODE=NL

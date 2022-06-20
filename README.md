de656881a7450ea6730aa98e14e55cd75ad8efa6
Freek Bes <freekbladnl@gmail.com>
2022-06-20 14:01:09 +0200

    munki installatiescript toegevoegd

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ## Overzicht
 
-**coach7install.bat:** installatiescript voor Coach 7, inclusief activatie met een bestaande licentie
+**coach7install.bat:** installatiescript voor Coach 7, inclusief activatie met een bestaande licentie.
 
-**startmenu2.bat:** script voor het toevoegen van software aan startmenu en bureaublad van leerlingenaccounts onder het Switch-netwerk
+**startmenu2.bat:** script voor het toevoegen van software aan startmenu en bureaublad van leerlingenaccounts onder het Switch-netwerk.
+
+**munkiinstall.sh:** script voor de installatie van Munki / Managed Software Center op mac van docent. Tip: voer uit op elke mac met `curl -L http://dmsc.freekb.es | sudo bash` in de Terminal.
---
+++ munkiinstall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then
+	echo "Error: no root access. Voer dit commando uit met sudo"
+	exit 1
+fi
+
+echo "Munki downloaden naar /tmp/munki.pkg..."
+curl -L -o /tmp/munki.pkg https://github.com/munki/munki/releases/download/v5.7.2/munkitools-5.7.2.4439.pkg
+
+echo "Munki installeren..."
+installer -pkg /tmp/munki.pkg -target /
+
+echo "Installer verwijderen..."
+rm -f /tmp/munki.pkg
+
+echo "Munki instellen voor Damstede Lyceum..."
+SETTINGS='/Library/Preferences/ManagedInstalls'
+defaults write $SETTINGS SoftwareRepoURL "smb://munki.damstede.eu/"
+defaults write $SETTINGS ClientIdentifier "mani_damstede"
+
+echo "Autorisatie instellen..."
+# Basic HTTP Authorization. Yes, the hashed user-pwd combination is visible here.
+# However, it is not much of a security risk, as this value is accessible to end users anyways.
+# The combinatation only gives the end user read-access anyways. No write-access anywhere.
+defaults write $SETTINGS AdditionalHttpHeaders -array "Authorization: Basic bXVua2lodHRwdXNlcjpSSzgzMTAyNUtNIQ=="
